bccc198234f070a85e9776ac314277a90a606204
Joe Bartlett <xo@redoPop.com>
2015-03-07 01:35:14 -0600

    A wee app to read notes from an HTTP location

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,93 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
+var ajax = require('ajax');
+var config = require('config/app.json');
 var UI = require('ui');
-var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+function showMenu() {
+  main.items(1, items);
+}
+
+function showNote(item) {
+  if (!item.card) {
+    item.card = new UI.Card({
+      title: item.title,
+      body: item.subtitle,
+      scrollable: true
+    });
+
+    item.card.on('click', 'back', function() {
+      item.card.hide();
+    });
+  }
+
+  item.card.show();
+}
+
+function parseNote(data) {
+  var dataArray = data.split('\n');
+  var item;
+
+  items = [];
+
+  for (var i = 0; i < dataArray.length; i++) {
+    if (dataArray[i][0] == '#') {
+      if (item) {
+        items.push(item);
+      }
+
+      item = {
+        title: dataArray[i].substr(2),
+        subtitle: ''
+      };
+    } else if (item) {
+      item.subtitle += '\n' + dataArray[i];
+    }
+  }
+
+  if (item) {
+    items.push(item);
+  }
+
+  showMenu();
+}
+
+function loadNote() {
+  ajax({
+    cache: false,
+    url: noteUrl
+  }, function (data) {
+    localStorage.setItem('note', data);
+    parseNote(data);
+  });
+}
+
+var data = localStorage.getItem('note');
+var noteUrl = config.noteUrl;
+var items = [];
+
+var main = new UI.Menu({
+  sections: [
+    {
+      items: [{title: 'Refresh'}]
+    },
+    {
+      title: 'Notes',
+      items: []
+    }
+  ]
+});
+
+main.on('select', function (e) {
+  if (e.sectionIndex === 1) {
+    showNote(items[e.itemIndex]);
+  } else {
+    loadNote();
+  }
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+if (data) {
+  parseNote(data);
+} else {
+  loadNote();
+}
